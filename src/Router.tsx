c8d664c9f165a3716fdaa9eaf2fd1957ba2ac04d
Daniel <danny2003renato@gmail.com>
2024-06-15 00:08:10 -0500

    Dev ops Test 8

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,6 @@
 //importamos Router para los path y el componente
 import { createHashRouter } from 'react-router-dom';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import style from './global.module.css'
 import Shop from './pages/Shop/Shop';
